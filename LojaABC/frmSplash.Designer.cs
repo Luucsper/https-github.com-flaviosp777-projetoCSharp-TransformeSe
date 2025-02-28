13cd150c610a281fad7e65f377866783305a6860
Luucsper <lucas.spereira59@senacsp.edu.br>
2025-02-28 17:21:52 -0300

    Janela de menu principal

--- LojaABC/frmSplash.Designer.cs
+++ LojaABC/frmSplash.Designer.cs
@@ -30,39 +30,78 @@ namespace LojaABC
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSplash));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pctLogo = new System.Windows.Forms.PictureBox();
+            this.lbNumero = new System.Windows.Forms.Label();
+            this.lblPorcentagem = new System.Windows.Forms.Label();
+            this.pgbSplash = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pctLogo)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // pctLogo
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(227, 135);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(244, 213);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.pctLogo.Image = ((System.Drawing.Image)(resources.GetObject("pctLogo.Image")));
+            this.pctLogo.Location = new System.Drawing.Point(264, 154);
+            this.pctLogo.Name = "pctLogo";
+            this.pctLogo.Size = new System.Drawing.Size(244, 213);
+            this.pctLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pctLogo.TabIndex = 0;
+            this.pctLogo.TabStop = false;
+            // 
+            // lbNumero
+            // 
+            this.lbNumero.AutoSize = true;
+            this.lbNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNumero.Location = new System.Drawing.Point(384, 384);
+            this.lbNumero.Name = "lbNumero";
+            this.lbNumero.Size = new System.Drawing.Size(18, 20);
+            this.lbNumero.TabIndex = 1;
+            this.lbNumero.Text = "0";
+            // 
+            // lblPorcentagem
+            // 
+            this.lblPorcentagem.AutoSize = true;
+            this.lblPorcentagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPorcentagem.Location = new System.Drawing.Point(397, 384);
+            this.lblPorcentagem.Name = "lblPorcentagem";
+            this.lblPorcentagem.Size = new System.Drawing.Size(23, 20);
+            this.lblPorcentagem.TabIndex = 2;
+            this.lblPorcentagem.Text = "%";
+            this.lblPorcentagem.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // pgbSplash
+            // 
+            this.pgbSplash.Location = new System.Drawing.Point(264, 413);
+            this.pgbSplash.Name = "pgbSplash";
+            this.pgbSplash.Size = new System.Drawing.Size(244, 23);
+            this.pgbSplash.TabIndex = 3;
             // 
             // frmSplash
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 561);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pgbSplash);
+            this.Controls.Add(this.lblPorcentagem);
+            this.Controls.Add(this.lbNumero);
+            this.Controls.Add(this.pctLogo);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "frmSplash";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "LojaABC - Splash";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Load += new System.EventHandler(this.frmSplash_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pctLogo)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pctLogo;
+        private System.Windows.Forms.Label lbNumero;
+        private System.Windows.Forms.Label lblPorcentagem;
+        private System.Windows.Forms.ProgressBar pgbSplash;
     }
 }
